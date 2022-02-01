a2049ec5d05d8aa800905f8a50ed54191c608e06
Nathan Hausman <hausmann@oregonstate.edu>
2022-01-31 16:08:54 -0800

    Added command line options to change host/port

--- website/server.js
+++ website/server.js
@@ -1,16 +1,32 @@
+// require express
 var express = require('express');
 
-var app = express()
-var port = process.env.PORT || 28070;
+// parse command line args
+var parseArgs = require('minimist');
+var argv = parseArgs(process.argv.slice(2), opts={
+    "string": "host",
+    "default": {"port": 28070}
+});
 
+// set up app and port
+var app = express()
+var port = process.env.PORT || argv["port"];
+
+// set up static files to serve
 app.use(express.static('public'));
 
+// set up routing
 app.get('/', function (req, res) {
     res.redirect(200, "/index.html");
-    
 });
 
-app.listen(port, function() {
-    console.log("== Server is listening on port ", port);
-});
-
+// open up server to traffic
+if ("host" in argv) {
+    app.listen(port, argv["host"], function() {
+        console.log("== Server is listening on port ", port);
+    });
+} else {
+    app.listen(port, function() {
+        console.log("== Server is listening on port ", port);
+    });
+}
